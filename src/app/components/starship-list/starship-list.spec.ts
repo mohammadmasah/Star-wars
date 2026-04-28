c6fc44a59ba8893eba722f9bef23e8075de37c06
Mohammad <mohammad-haroon.masah@epitech.eu>
2026-04-28 15:36:47 +0200

    test: add unit tests for SWAPI service and StarshipList component
    - Add 4 unit tests for StarWarsService (pagination logic, search query, last page detection)
    - Add 4 unit tests for StarshipList component (editing behavior, localStorage, search reset)
    - All 10 tests passing

--- src/app/components/starship-list/starship-list.spec.ts
+++ src/app/components/starship-list/starship-list.spec.ts
@@ -1,22 +1,83 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AgGridAngular } from 'ag-grid-angular';
 import { StarshipList } from './starship-list';
+import { StarWarsService } from '../../services/star-wars.service';
+import { of } from 'rxjs';
 
-describe('StarshipList', () => {
+describe('StarshipList Component', () => {
   let component: StarshipList;
   let fixture: ComponentFixture<StarshipList>;
+  let mockSwService: any;
+
+  const mockResponse = {
+    count: 37,
+    next: null,
+    previous: null,
+    results: [
+      {
+        name: 'Millennium Falcon',
+        model: 'YT-1300',
+        manufacturer: 'Corellian Engineering',
+        crew: '4',
+        passengers: '6',
+        hyperdrive_rating: '0.5',
+      },
+    ],
+  };
 
   beforeEach(async () => {
+    mockSwService = {
+      getStarships: () => of(mockResponse),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [StarshipList],
+      imports: [HttpClientTestingModule, AgGridAngular, StarshipList],
+      providers: [{ provide: StarWarsService, useValue: mockSwService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StarshipList);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should save edited cell value to localStorage', () => {
+    const mockEvent = {
+      data: {
+        name: 'Millennium Falcon',
+        model: 'YT-1300 f Corellian Freighter',
+        manufacturer: 'Corellian Engineering',
+        crew: '4',
+        passengers: '6',
+        hyperdrive_rating: '0.5',
+      },
+    };
+
+    component.onCellValueChanged(mockEvent);
+
+    const saved = localStorage.getItem('starship_edits');
+    expect(saved).not.toBeNull();
+
+    const edits = JSON.parse(saved!);
+    expect(edits['Millennium Falcon'].model).toBe('YT-1300 f Corellian Freighter');
+  });
+
+  it('should clear localStorage on reset', () => {
+    localStorage.setItem('starship_edits', JSON.stringify({ test: true }));
+    localStorage.removeItem('starship_edits');
+    expect(localStorage.getItem('starship_edits')).toBeNull();
+  });
+
+  it('should reset isEndOfList to false when search changes', () => {
+    component.isEndOfList = true;
+
+    const mockEvent = { target: { value: 'falcon' } };
+    component.onSearch(mockEvent);
+
+    expect(component.isEndOfList).toBe(false);
   });
 });
--- src/app/services/star-wars.spec.ts
+++ src/app/services/star-wars.spec.ts
@@ -1,16 +1,89 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { StarWarsService } from './star-wars.service';
 
-import { StarWars } from './star-wars';
-
-describe('StarWars', () => {
-  let service: StarWars;
+describe('StarWarsService', () => {
+  let service: StarWarsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StarWars);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StarWarsService],
+    });
+    service = TestBed.inject(StarWarsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch page 1 by default', () => {
+    const mockResponse = {
+      count: 37,
+      next: 'https://swapi.py4e.com/api/starships/?page=2',
+      previous: null,
+      results: [{ name: 'Millennium Falcon' }],
+    };
+
+    service.getStarships().subscribe((response) => {
+      expect(response.count).toBe(37);
+      expect(response.results.length).toBe(1);
+      expect(response.next).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne('https://swapi.py4e.com/api/starships/?page=1&search=');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should fetch correct page when paginating', () => {
+    const mockPage2 = {
+      count: 37,
+      next: 'https://swapi.py4e.com/api/starships/?page=3',
+      previous: 'https://swapi.py4e.com/api/starships/?page=1',
+      results: [{ name: 'Star Destroyer' }],
+    };
+
+    service.getStarships(2).subscribe((response) => {
+      expect(response.results[0].name).toBe('Star Destroyer');
+      expect(response.previous).toBeTruthy();
+      expect(response.next).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne('https://swapi.py4e.com/api/starships/?page=2&search=');
+    req.flush(mockPage2);
+  });
+
+  it('should return null for next on last page', () => {
+    const mockLastPage = {
+      count: 37,
+      next: null,
+      previous: 'https://swapi.py4e.com/api/starships/?page=3',
+      results: [{ name: 'Scimitar' }],
+    };
+
+    service.getStarships(4).subscribe((response) => {
+      expect(response.next).toBeNull();
+    });
+
+    const req = httpMock.expectOne('https://swapi.py4e.com/api/starships/?page=4&search=');
+    req.flush(mockLastPage);
+  });
+
+  it('should include search query in request URL', () => {
+    const mockSearchResponse = {
+      count: 1,
+      next: null,
+      results: [{ name: 'Millennium Falcon' }],
+    };
+
+    service.getStarships(1, 'falcon').subscribe((response) => {
+      expect(response.results[0].name).toBe('Millennium Falcon');
+    });
+
+    const req = httpMock.expectOne('https://swapi.py4e.com/api/starships/?page=1&search=falcon');
+    req.flush(mockSearchResponse);
   });
 });
